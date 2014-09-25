85a3aea210d99aab3abb8d11b960f7a38913d29d
Andrea Nodari <andrea.nodari91@gmail.com>
2014-09-25 16:23:13 +0300

    model is getting serius

--- model.js
+++ model.js
@@ -1,41 +1,20 @@
 // http://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
 
-var orb = require('orbjs');
-
-var lat1 = 45.0;
-var lon1 = 11.0;
-
-var lat2 = 45.1;
-var lon2 = 11.1;
-
-var earthRadius = 6378.1;
-var ellipticity = 0.00335;
-var height = 0;
-
-
-//geodedic
-var cartesian1 = orb.transformations.geodeticToCartesian(
-    [lat1, lon1, earthRadius, height, ellipticity]
-);
-
-var cartesian2 = orb.transformations.geodeticToCartesian(
-    [lat2, lon2, earthRadius, height, ellipticity]
-);
-
-//spherical
-var cartesian1s = orb.transformations.sphericalToCartesian(
-    [lat1, lon1, earthRadius]
-);
-
-var cartesian2s = orb.transformations.sphericalToCartesian(
-    [lat2, lon2, earthRadius]
-);
+var coordinator = require('coordinator');
+var fromLatLong = coordinator('latlong', 'utm');
+var toLatLong = coordinator('utm', 'latlong');
 
 var computeLine = function (x1, y1, x2, y2) {
     var slope = (y1-y2)/(x1-x2);
     var intercept = (x1*y2 - x2*y1)/(x1-x2);
-    return [slope, intercept]
+    var delta_x = x2 - x1;
+    var delta_y = y2 - y1;
+    var distance = Math.sqrt(delta_x*delta_x + delta_y*delta_y);
+    var angle = Math.acos(delta_x / distance);
+    return [slope, intercept, angle, distance]
 }
 
-var line = computeLine(cartesian1[0], cartesian1[1], cartesian2[0], cartesian2[1])
-console.log(line);
+var nextPoint = function (x_last, y_last, angle, speed, time) {
+    var space = speed * time;
+    return [x_last + space*Math.cos(angle), y_last + space*Math.sin(angle)];
+}
